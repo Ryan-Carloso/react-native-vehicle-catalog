b9d8267725932cf40b53ed71780e2870b5aec247
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2025-02-17 09:03:00 +0000

    fix: project setup and dependencies

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.watchFolders = [path.resolve(__dirname, '..')];
+config.watchFolders = [...config.watchFolders, path.resolve(__dirname, '..')];
 
 config.resolver.nodeModulesPaths = [
   path.resolve(__dirname, 'node_modules'),
@@ -14,6 +14,7 @@ config.resolver.alias = {
   '@': path.resolve(__dirname, '.'),
   '@/assets': path.resolve(__dirname, 'assets'),
   '@/src': path.resolve(__dirname, 'src'),
+  '@shared': path.resolve(__dirname, '../shared'),
 };
 
 module.exports = config;
